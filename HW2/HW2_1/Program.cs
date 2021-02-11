06a4c5ebb356b09219178193ebb51b4704e27f73
LobJay <roach42ful@gmail.com>
2021-02-11 20:53:00 +0300

    Add HW2, task 1

---
+++ HW2/HW2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace HW2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Прочитать файл по указанному пути
+            //Создать массив, в котором будет храниться прочитанная информация
+            //Считать информацию из ранее созданного массива
+            //Конвертировать информацию
+            //Создать картинку
+
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
+            textReader.Dispose();
+        }
+    }
+}

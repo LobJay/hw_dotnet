3a24974743f30e03c3299d63405b4b62c70e3be8
LobJay <roach42ful@gmail.com>
2021-02-11 23:30:07 +0300

    Add HW2, task 4

---
+++ HW2/HW2_4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 12;
+            Int32 a2 = 13;
+            Console.WriteLine(a1);
+            Console.WriteLine(a2);
+
+            sbyte a3 = 3;
+            SByte a4 = 4;
+            Console.WriteLine(a3);
+            Console.WriteLine(a4);
+
+            float a5 = 5.55f;
+            Single a6 = 6.66f;
+            Console.WriteLine(a5);
+            Console.WriteLine(a6);
+
+            string a7 = "Hello";
+            String a8 = "Hello";
+            Console.WriteLine(a7);
+            Console.WriteLine(a8);
+
+            object a9 = new object();
+            Object a10 = new object();
+            Console.WriteLine(a9);
+            Console.WriteLine(a10);
+
+            short b1 = 1;
+            Int16 b2 = -1;
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+
+            ulong b3 = 1;
+            UInt64 b4 = 2;
+            Console.WriteLine(b3);
+            Console.WriteLine(b4);
+        }
+    }
+}

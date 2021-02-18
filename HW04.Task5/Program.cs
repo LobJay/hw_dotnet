8fdc2d3007ff900d21c37a411347dbf9f6195f2f
LobJay <roach42ful@gmail.com>
2021-02-18 14:28:47 +0300

    Add HW04.Task5

---
+++ HW04.Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int az = 122;
+            for (int i = 0; i < 26; i++)
+            {
+                Console.WriteLine((char)az);
+                az--;
+            }
+        }
+    }
+}

d33a3c066c251fc791a4676dc8f55866160efbca
LobJay <roach42ful@gmail.com>
2021-02-23 17:10:30 +0300

    Add HW05 Task 01

---
+++ HW.05.Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW._05.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a positive number: ");
+            string userInput = Console.ReadLine();
+
+            int answer = 0;
+
+            if (int.TryParse(userInput, out int userNum))
+            {
+                for (int i = 1; i <= userNum; i++)
+                {
+                    answer += i;
+                }
+                Console.WriteLine(answer);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number. Try again:");               
+            }
+        } 
+    }
+}

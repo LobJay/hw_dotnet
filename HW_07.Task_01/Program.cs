51850d328b6e25225f46130db2a090b69b9fe686
LobJay <roach42ful@gmail.com>
2021-03-11 22:29:00 +0300

    Add HW_07.Task_01

---
+++ HW_07.Task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW_07.Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your poem. Symbol \";\" separate strings");
+            string userpoem = Console.ReadLine();
+            string poemA = userpoem.Replace('о', 'а');
+
+            PrintPoem(poemA);
+        }
+        static void PrintPoem(string poem)
+        {           
+            string[] separate = poem.Split(';');
+            foreach (string newpoem in separate)
+            {
+                Console.WriteLine(newpoem);
+            }
+        }
+    }
+}
